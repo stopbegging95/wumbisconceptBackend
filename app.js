702f1317828bd0414e2fbd093ae002077cc659e4
Umar Ibrahim Ayobami (stopbegging95) <stopbeging@gmail.com>
2024-09-10 12:07:07 +0100

    methods uncomment

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const path = require("path")
 app.use(cors({
   //origin: 'http://localhost:3000',
   origin: ['https://wumbisdeployment.vercel.app/',],
-  // methods: ["GET","POST","PUT","DELETE"], 
+  methods: ["GET","POST","PUT","DELETE", "PATCH"], 
   credentials: true
 }));
 
