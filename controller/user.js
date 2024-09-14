0e3c70cf746035a739099e4dbf4c9d9db8d29567
Umar Ibrahim Ayobami (stopbegging95) <stopbeging@gmail.com>
2024-09-14 10:29:14 +0100

    Update user.js

--- controller/user.js
+++ controller/user.js
@@ -39,7 +39,7 @@ router.post("/create-user", async (req, res, next) => {
 
     const activationToken = createActivationToken(user);
 
-  // const activationUrl = `http://localhost:3000/activation/${activationToken}`;
+   //const activationUrl = `http://localhost:3000/activation/${activationToken}`;
   const activationUrl = `https://wumbisdeployment.vercel.app/activation/${activationToken}`;
 
     try {
